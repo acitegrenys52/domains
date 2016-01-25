3a6c466edc0725c4059b4b3e58cc2752883a8909
ArtikUA <artik.ua@gmail.com>
2016-01-25 17:34:29 +0200

    permissions for update fixed

--- app/permissions.py
+++ app/permissions.py
@@ -6,10 +6,10 @@ from app.constants import ALLOWED_LOGIN
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         print(request.method)
-        if request.method == "POST":
-            return str(request.user) == ALLOWED_LOGIN
-        elif request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
+        elif request.method in ('POST', 'PATCH', 'PUT'):
+            return str(request.user) == ALLOWED_LOGIN
         return False
 
     def has_object_permission(self, request, view, obj):
